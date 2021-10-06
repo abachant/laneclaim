564cdd6981a34f0223b4c9a3cbcd5b9eb38acd5e
abachant <aubreybachant@gmail.com>
2021-10-06 16:43:39 -0400

    Update ARIA tab compatibility for Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,15 +1,21 @@
 import React, { FC } from 'react';
 
-const Nav: FC = ({ toggleAboutOpen }) => {
-  return (
-    <nav className="nav">
-      <h2 className="nav__item" >Laneclaim</ h2>
-      <div className="nav__list">
-        <div className="nav__item nav__item--link" onClick={toggleAboutOpen}>about</div>
-        <button className="nav__item" type="button">Submit New Claim</button>
+const Nav: FC = ({ toggleAboutOpen }) => (
+  <nav className="nav">
+    <h2 className="nav__item" >Laneclaim</ h2>
+    <div className="nav__list">
+      <div
+        className="nav__item nav__item--link"
+        role="button"
+        onClick={toggleAboutOpen}
+        tabIndex={0}
+        onKeyDown={toggleAboutOpen}
+      >
+        about
       </div>
-    </nav>
-  );
-};
+      <button className="nav__item" type="button">Submit New Claim</button>
+    </div>
+  </nav>
+);
 
 export default Nav;
