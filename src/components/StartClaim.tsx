44ca78dfef4d2405a33f7fae11915503e9c70d97
abachant <aubreybachant@gmail.com>
2021-11-10 00:40:03 -0500

    Fix naming convention of input on StartClaim

--- src/components/StartClaim.tsx
+++ src/components/StartClaim.tsx
@@ -19,7 +19,7 @@ const StartClaim = ({ isOpen, toggleStartClaimOpen, toggleEditClaimOpen }: Props
     <div>
       <h2 className="modal__title">Submit New Claim</h2>
       <h6>Upload a photo of a vehicle obstructing a bike lane</h6>
-      <input type="file" name="claim-file" id="photo-file-input" accept="image/.jpeg" />
+      <input type="file" name="start-claim__file" id="start-claim__input" accept="image/.jpeg" />
       <p>File must be a jpeg with exif data</p>
       <button type="button" onClick={toggleStartClaimOpen}>
         Close
